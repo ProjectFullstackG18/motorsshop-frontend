d8b67a706c3611c4ba24a60f193f54fdb814b0ad
drmatheus <davilamatheus@outlook.com>
2023-06-09 09:21:10 -0300

    feat: gallery component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Footer } from '@/components/footer/footer'
-import { Header } from '@/components/header/header'
-import { Button } from '@/components/button/button'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Footer } from "@/components/footer/footer";
+import { Header } from "@/components/header/header";
+import { Button } from "@/components/button/button";
+import { Gallery } from "@/components/gallery/gallery";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
       <Header>
-        <Button text='Fazer Login' type='button' className="font-semibold"/>
-        <Button text='Cadastrar' type='button' className="border-2 border-gray-500 rounded w-36 h-10 font-semibold"/>
+        <Button text="Fazer Login" type="button" className="font-semibold" />
+        <Button
+          text="Cadastrar"
+          type="button"
+          className="border-2 border-gray-500 rounded w-36 h-10 font-semibold"
+        />
       </Header>
+      {/* <Gallery></Gallery> */}
       {/* <Footer/> */}
     </main>
-  )
+  );
 }
