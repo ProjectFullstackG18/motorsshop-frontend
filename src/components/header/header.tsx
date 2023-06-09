16bfafa853a9c3e77c7a0dd889d609e907c655e3
Eduardo <eduardo.portela.jrr@gmail.com>
2023-06-09 15:07:53 -0300

    fix: gradient logo header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,8 +12,8 @@ export const Header = ({children}: IHeaderProps) => {
   return (
     <header className='h-20 border-b-2'>
         <div className=' pl-2 pr-2 max-w-screen-2xl mx-auto flex justify-between items-center h-full'>
-            <h2 className='text-3xl font-bold'>Motors <span className='text-lg text-purple-700'>Shop</span></h2>
-            <div className={`md:flex  duration-500 ${open ? `flex absolute w-full justify-evenly p-2 m-0 border-b-2 border-l-0 duration-500 mt-40` :  "hidden h-full duration-500 top-[-100%] "} items-center justify-between gap-8 border-l-2 border-gray-300 pl-16`}>
+            <h2 className='bg-gradient-to-l from-purple-800 to-black text-transparent bg-clip-text text-3xl font-bold'>Motors <span className='text-lg'>Shop</span></h2>
+            <div className={`md:flex  duration-500 ${open ? `flex absolute w-full justify-evenly p-2 m-0 border-b-2 border-l-white duration-500 mt-40` :  "hidden h-full duration-500 top-[-100%] "} items-center justify-between gap-8 border-l-2 pl-16`}>
                 {children}
             </div>
             <div className='md:hidden flex' onClick={() => setOpen(!open)}>
