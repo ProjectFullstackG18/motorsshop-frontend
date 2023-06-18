28ead79e761989810e2bca88303f2c2c2b5d517b
drmatheus <davilamatheus@outlook.com>
2023-06-18 09:49:29 -0300

    feat: modal form create and update car infos

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,42 +1,42 @@
-import { InputHTMLAttributes } from "react";
+import { ChangeEvent, InputHTMLAttributes, ReactNode } from "react";
 
 interface IInputProps {
   labelName: string;
   labelText: string;
   placeholder: string;
-  type: string;
   register?: any;
   disable?: boolean;
-  value?: string;
-  defaultValue?: string;
+  children: ReactNode;
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
-export const Input = ({
+export const Select = ({
   labelName,
   labelText,
   placeholder,
-  type,
   register,
   disable,
-  value,
-  defaultValue,
+  children,
+  onChange,
 }: IInputProps) => {
   return (
     <>
       <label className="-mb-6 text-sm font-medium" htmlFor={labelName}>
         {labelText}
       </label>
-      <input
+      <select
         className="rounded px-3 h-12 border-2 border-grey8 bg-grey11 text-sm outline-grey3"
         disabled={disable}
         required
-        placeholder={placeholder}
         name={labelName}
-        type={type}
         {...register}
-        value={value}
-        defaultValue={defaultValue}
-      />
+        onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+          if (onChange) onChange(e);
+        }}
+      >
+        <option value={""}>{placeholder}</option>
+        {children}
+      </select>
     </>
   );
 };
--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -4,3 +4,21 @@ export const api = axios.create({
   baseURL: "http://localhost:3000/",
   timeout: 5000,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("motorshop@token");
+    if (token) {
+      config.headers.Authorization = JSON.parse(token).token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const apiCars = axios.create({
+  baseURL: "https://kenzie-kars.herokuapp.com/",
+  timeout: 5000,
+});
