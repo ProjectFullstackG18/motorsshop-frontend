400bc544b9ba0b43edda03a6966814561eaa12eb
PzLeMoNBR <marciogabrieloficial@hotmail.com>
2023-06-22 10:57:35 -0300

    fix : minnior bug in input

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -61,20 +61,25 @@ export const Input = ({
           />
         </>
       ) : (
-        <>
-          <label className="-mb-6 text-sm font-medium" htmlFor={labelName}>
-            {labelText}
-          </label>
-          <input
-            className="rounded px-3 h-12 border-2 border-grey8 bg-grey11 text-sm outline-grey3"
-            disabled={disable}
-            required
-            placeholder={placeholder}
-            name={labelName}
-            type={type}
-            {...register}
-          />
-        </>
+        <input
+          className={[
+            "rounded",
+            "px-3",
+            "h-12",
+            "border-2",
+            "border-grey8",
+            "bg-grey11",
+            "text-sm",
+            "outline-grey3",
+            classInput,
+          ].join(" ")}
+          disabled={disable}
+          required
+          placeholder={placeholder}
+          name={labelName}
+          type={type}
+          {...register}
+        />
       )}
     </>
   );
